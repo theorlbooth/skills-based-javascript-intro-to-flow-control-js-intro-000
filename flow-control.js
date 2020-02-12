3410d88094f57aa57cff1820ad69b1335dfb3790
Theo Booth <theorlbooth@googlemail.com>
2020-02-12 16:50:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,3 +25,30 @@ function ageChecker(age) {
 function ternaryTeenager(age) {
   return (age >= 13 && age <= 19 ? "You are a teenager" : "You are not a teenager")
 }
+
+function switchAge(age) {
+  switch (age) {
+    case 13:
+      console.log("You are a teenager")
+      break;
+    case 14:
+      console.log("You are a teenager")
+      break;
+    case 15:
+      console.log("You are a teenager")
+      break;
+    case 16:
+      console.log("You are a teenager")
+      break;
+    case 17:
+      console.log("You are a teenager")
+      break;
+    case 18:
+      console.log("You are a teenager")
+      break;
+    case 19:
+      console.log("You are a teenager")
+      break;
+    default:
+      console.log("You have an age")
+}
